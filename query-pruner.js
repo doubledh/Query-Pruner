6076b7b39d5f610a9971bc4adfc1db14de027154
Henry Hu <henryh2014@gmail.com>
2017-08-04 08:59:04 -0700

    update

--- query-pruner.js
+++ query-pruner.js
@@ -1,54 +1,65 @@
-function prune(obj, query) {
+const ID_words = "name|identifier|\\w{0,2}id|(?:\\w+\\s)?code".split("|");
+function prune(obj, query, identifier_words = ID_words) {
   let query_array = query.split(" ");
   let result = JSON.parse(JSON.stringify(obj));
   let obj_stack = [result];
   while (obj_stack.length) {
-    let current_obj = obj_stack.pop();
+    let current_obj = obj_stack.shift();
     // console.log(current_obj);
     if (Array.isArray(current_obj)) {
       // console.log("array");
-      let any = current_obj.some(e => has_relevant_children(e, query));
-      current_obj.forEach((e, i) => {
-        if (any && !has_relevant_children(e, query)) {
+      let query_array2 = query_array.concat(ID_words);
+
+      let any_children = current_obj.some(e => has_relevant_children(e, query_array2));
+      for(var i = 0; i<current_obj.length; i = i+1){
+        // console.log("here");
+        if (any_children && !has_relevant_children(current_obj[i], query_array2)) {
           current_obj.splice(i, 1);
+          i = i-1;
         }
-        else
-          obj_stack.push(e);
-      });
+        else {
+          obj_stack.push(current_obj[i]);
+        }
+      }
     }
     else if (current_obj === Object(current_obj)) {
-      //console.log("object");
-      let any = has_relevant_children(current_obj, query);
-      //console.log(`any:${any}`)
+      // console.log("object");
+      let keys = Object.keys(current_obj);
+      let any_key = keys.some(key => includes_which(key, query_array).length);
+      let any_children = has_relevant_children(current_obj, query_array);
       for (var key in current_obj) {
-        if (any && !has_relevant_children(current_obj[key], query, key)) {
+        if (any_key && !includes_which(key, query_array).length) {
+          // console.log(`immediate key ${key} is not in ${query_array}`);
+          delete current_obj[key];
+        }
+        else if (any_children && !has_relevant_children(current_obj[key], query_array, key)) {
+          // console.log(`no descendants of ${key} ${current_obj[key]} has ${query}`)
           delete current_obj[key];
         }
         else {
+          // console.log(`key ${key} is safe`);
           obj_stack.push(current_obj[key]);
         }
       }
-
     }
     else {
       // console.log("primitive");
       continue;
     }
-
   }
   return result;
 }
 
-function has_relevant_children(obj, query_string, obj_name = "") {
-  let query_array = query_string.split(/\W+/).filter(s=>s!=""),
-  root = (obj_name? obj_name+":": "") + JSON.stringify(obj);
-  root = root.replace(/([A-Z])/g," $1"); // deals with camel-case
+function has_relevant_children(obj, query_array, obj_name = "") {
+  var root = (obj_name ? obj_name + ":" : "") + JSON.stringify(obj);
+  root = root.replace(/([A-Z])/g, " $1"); // deals with camel-case
   // console.log(root);
   // console.log(query_array)
   return includes_which(root, query_array).length > 0;
 }
 function includes_which(string, criteria) {
-  return criteria.filter(c =>  string.match(RegExp(`.*\\b${c}\\b.*`,"i"))!=null);
+  return criteria.filter(c => 
+    string.match(RegExp(`.*\\b${c}\\b.*`, "i")) != null);
 }
 function sub_intersect(strings1, strings2) {
   return strings1.filter(s1 => includes_which(s1, s2));
--- test1.js
+++ test1.js
@@ -4,7 +4,7 @@ var exampleJSON = {
   str: "hi", // fetch by key
   obj: { z: 1, y: 10, in1:{in2:{in3:{in4:{in5:{in6:1}}}}}}, // fetch by key
   arrayVals: [1, 10, 20], // enum, fetch by key-->index?
-  arrayObj: [{ a: 1, b: 2 }, { a: 4, b: 6 }, { a: 11, b: 13 }], // fetch by value: "where"
+  arrayObj: [{ name:"joe", a: 1, b: 2 }, { guid: 12345, a: 4, b: 6 }, { name:12346, a: 11, z: 13 }], // fetch by value: "where"
   arrayArray: [[1, 2, 3], [2, 3, 4], [4, 5, 6]] // e
 
 }
@@ -17,5 +17,5 @@ console.log("pruning; protect in6 key:");
 console.log(prune(exampleJSON, "in6"));
 console.log("pruning; protect object with value of 6");
 console.log(prune(exampleJSON,"6"));
-console.log("pruning; protect object with val of 'hi' ");
-console.log(prune(exampleJSON,"hi"));
+console.log("pruning; protect object with val of 'z' ");
+console.log(prune(exampleJSON,"z"));
